22cfa49efa9e4402d18726749aa7d14398a18956
Ryan Efendy <ryanefendy95@gmail.com>
2017-09-05 08:50:37 -0700

    add and fix experience section

--- src/components/Experiece.js
+++ src/components/Experiece.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Card, Container, Header, Divider, Grid } from 'semantic-ui-react';
-
-const description = [
-  'Amy is a violinist with 2 years experience in the wedding industry.',
-  'She enjoys the outdoors and currently resides in upstate New York.'
-].join(' ');
+import {
+  Card,
+  Container,
+  Header,
+  Divider,
+  Grid,
+  Feed
+} from 'semantic-ui-react';
 
 const Experience = () => (
   <div
@@ -15,34 +17,186 @@ const Experience = () => (
   >
     <Container textAlign="center">
       <Header as="h2" textAlign="center" content="Experience" />
+
       <Divider section />
       <Grid relaxed columns={4}>
         <Grid.Column>
           <Card>
-            <Card.Content header="About Amy" />
-            <Card.Content description={description} />
-            <Card.Content extra />
+            <Card.Content>
+              <Card.Header>Site Reliability Engineer At PayPal</Card.Header>
+              <Card.Meta>Jun 2017 – Present</Card.Meta>
+            </Card.Content>
+            <Card.Content>
+              <Feed>
+                <Feed.Event>
+                  <Feed.Content>
+                    <Feed.Summary>
+                      ★ Implemented form components to collect user input to be
+                      passed to a Node.js, Express and MongoDB back end to
+                      triage tickets based on priority level and make
+                      appropriate decisions
+                    </Feed.Summary>
+                  </Feed.Content>
+                </Feed.Event>
+
+                <Feed.Event>
+                  <Feed.Content>
+                    <Feed.Summary>
+                      ★ Developed new features using React and Redux and built
+                      reusable components and front-end libraries for future use
+                      such as input components that are shared in more than 10
+                      pages of the applications
+                    </Feed.Summary>
+                  </Feed.Content>
+                </Feed.Event>
+
+                <Feed.Event>
+                  <Feed.Content>
+                    <Feed.Summary>
+                      ★ Refactored Grunt build tasks to decrease build times
+                      from 30 seconds to less than 5 seconds in a React, Node
+                      project by separating the tasks and enabled live reloading
+                      of updated files to the browser
+                    </Feed.Summary>
+                  </Feed.Content>
+                </Feed.Event>
+              </Feed>
+            </Card.Content>
           </Card>
         </Grid.Column>
+
         <Grid.Column>
           <Card>
-            <Card.Content header="About Amy" />
-            <Card.Content description={description} />
-            <Card.Content extra />
+            <Card.Content>
+              <Card.Header>Site Reliability Engineer At PayPal</Card.Header>
+              <Card.Meta>Jun 2017 – Present</Card.Meta>
+            </Card.Content>
+            <Card.Content>
+              <Feed>
+                <Feed.Event>
+                  <Feed.Content>
+                    <Feed.Summary>
+                      ★ Implemented form components to collect user input to be
+                      passed to a Node.js, Express and MongoDB backend to triage
+                      tickets based on priority level and make appropriate
+                      decisions
+                    </Feed.Summary>
+                  </Feed.Content>
+                </Feed.Event>
+
+                <Feed.Event>
+                  <Feed.Content>
+                    <Feed.Summary>
+                      ★ Developed new features using React and Redux and built
+                      reusable components and front-end libraries for future use
+                      such as input components that are shared in more than 10
+                      pages of the applications
+                    </Feed.Summary>
+                  </Feed.Content>
+                </Feed.Event>
+
+                <Feed.Event>
+                  <Feed.Content>
+                    <Feed.Summary>
+                      ★ Refactored Grunt build tasks to decrease build times
+                      from 30 seconds to less than 5 seconds in a React, Node
+                      project by separating the tasks and enabled live reloading
+                      of updated files to the browser
+                    </Feed.Summary>
+                  </Feed.Content>
+                </Feed.Event>
+              </Feed>
+            </Card.Content>
           </Card>
         </Grid.Column>
+
         <Grid.Column>
           <Card>
-            <Card.Content header="About Amy" />
-            <Card.Content description={description} />
-            <Card.Content extra />
+            <Card.Content>
+              <Card.Header>Site Reliability Engineer At PayPal</Card.Header>
+              <Card.Meta>Jun 2017 – Present</Card.Meta>
+            </Card.Content>
+            <Card.Content>
+              <Feed>
+                <Feed.Event>
+                  <Feed.Content>
+                    <Feed.Summary>
+                      ★ Implemented form components to collect user input to be
+                      passed to a Node.js, Express and MongoDB backend to triage
+                      tickets based on priority level and make appropriate
+                      decisions
+                    </Feed.Summary>
+                  </Feed.Content>
+                </Feed.Event>
+
+                <Feed.Event>
+                  <Feed.Content>
+                    <Feed.Summary>
+                      ★ Developed new features using React and Redux and built
+                      reusable components and front-end libraries for future use
+                      such as input components that are shared in more than 10
+                      pages of the applications
+                    </Feed.Summary>
+                  </Feed.Content>
+                </Feed.Event>
+
+                <Feed.Event>
+                  <Feed.Content>
+                    <Feed.Summary>
+                      ★ Refactored Grunt build tasks to decrease build times
+                      from 30 seconds to less than 5 seconds in a React, Node
+                      project by separating the tasks and enabled live reloading
+                      of updated files to the browser
+                    </Feed.Summary>
+                  </Feed.Content>
+                </Feed.Event>
+              </Feed>
+            </Card.Content>
           </Card>
         </Grid.Column>
+
         <Grid.Column>
           <Card>
-            <Card.Content header="About Amy" />
-            <Card.Content description={description} />
-            <Card.Content extra />
+            <Card.Content>
+              <Card.Header>Site Reliability Engineer At PayPal</Card.Header>
+              <Card.Meta>Jun 2017 – Present</Card.Meta>
+            </Card.Content>
+            <Card.Content>
+              <Feed>
+                <Feed.Event>
+                  <Feed.Content>
+                    <Feed.Summary>
+                      ★ Implemented form components to collect user input to be
+                      passed to a Node.js, Express and MongoDB backend to triage
+                      tickets based on priority level and make appropriate
+                      decisions
+                    </Feed.Summary>
+                  </Feed.Content>
+                </Feed.Event>
+
+                <Feed.Event>
+                  <Feed.Content>
+                    <Feed.Summary>
+                      ★ Developed new features using React and Redux and built
+                      reusable components and front-end libraries for future use
+                      such as input components that are shared in more than 10
+                      pages of the applications
+                    </Feed.Summary>
+                  </Feed.Content>
+                </Feed.Event>
+
+                <Feed.Event>
+                  <Feed.Content>
+                    <Feed.Summary>
+                      ★ Refactored Grunt build tasks to decrease build times
+                      from 30 seconds to less than 5 seconds in a React, Node
+                      project by separating the tasks and enabled live reloading
+                      of updated files to the browser
+                    </Feed.Summary>
+                  </Feed.Content>
+                </Feed.Event>
+              </Feed>
+            </Card.Content>
           </Card>
         </Grid.Column>
       </Grid>
