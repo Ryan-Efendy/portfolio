f03396f9faaa01b266f61dc46e365c210e6f3840
Ryan Efendy <ryanefendy95@gmail.com>
2019-01-30 23:38:10 -0800

    Updates

---
+++ precache-manifest.cd4b4f6e6fddc4ae2faac6af3795ea24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b3a73b54924fadc533a",
+    "url": "/portfolio/static/css/main.09362b92.chunk.css"
+  },
+  {
+    "revision": "4b3a73b54924fadc533a",
+    "url": "/portfolio/static/js/main.4b3a73b5.chunk.js"
+  },
+  {
+    "revision": "f5ca128d97dcc94e4ca0",
+    "url": "/portfolio/static/js/1.f5ca128d.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fd97cd8bdcf30cefab065400e9aea2b5",
+    "url": "/portfolio/index.html"
+  }
+];
